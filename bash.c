b6706c6e7df1322c5a8cd5ddb47e0eb6ae23e0cb
Abdulqader <aboooddada87@gmail.com>
2022-04-15 05:30:26 +0300

    implemented execvp to run any program

--- bash.c
+++ bash.c
@@ -4,6 +4,83 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+/* bulitin commands section */
+    // prompt printing function
+
+
+    // current working directory
+    char *pwd_command() {
+        char *cwd = (char *)calloc(512, sizeof(char));
+        getcwd(cwd, 512);
+        return cwd;
+    }
+
+    // command reader, returns an array of commands & args(command is 1st element)
+    char **read_command() {
+        char string[512]; /* initially input consist of 512 char but you need to change it to be dynamic */
+        scanf(" %[^\n]s", string);
+        char *token = strtok(string, " "), **string_list = (char **)malloc(10 * sizeof(char)); /* (string_list)initially the bassh accepts 1 command and 9 args */
+        for (int index = 0; token != NULL; index++)
+        {
+            string_list[index] = strdup(token);
+            token = strtok(NULL, " ");
+        }
+        return string_list;
+    }
+
+    // change directory command [returns -1 on fail, zero on success]
+    int cd(char *path) {
+        return chdir(path);
+    }
+
+    // [returns current process ID]
+    int pid() {
+      return getpid();
+    }
+
+    // [returns Parent process ID
+    int ppid() {
+      return getppid();
+    }
+
+    // set enviroment variable [returns -1 on fail, zero on success]
+    int set(char *var_name, char *val) {
+        return setenv(var_name, val, 1);
+    }
+
+    char *get(char *var_name){
+        return getenv(var_name);
+    }
+
+    /* extra */
+        void clear() {
+            system("clear");
+        }
+
+        // unset enviroment variable [returns -1 on fail, zero on success]
+        int unset(char *var_name) {
+            return unsetenv(var_name);
+        }
+
+/* non-bulitin commands */
+pid_t spawn_ch_process(const char* program, char** arg_list) {
+    pid_t ch_pid = fork();
+    if (ch_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ch_pid > 0) {
+        printf("\033[38;2;226;125;95;1m** \033[0mspawn child with pid - %d\033[38;2;226;125;95;1m **\033[0m\n", ch_pid);
+        return ch_pid;
+    } else {
+        execvp(program, arg_list);
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+}
 
 /* prompt setter */
   // type_prompt {done}
@@ -21,6 +98,8 @@
     // get {done}
 
     /* extra */
+        // jobs
+        // ">" redirect output
         // clear {done}
         // unset {done}
 
@@ -29,81 +108,24 @@
 
 int main(int argc, char *argv[])
 {
-    /* bulitin commands section */
-        // prompt printing function
-        void type_prompt(char *wd) {
-            if (argc == 3)
+    void type_prompt(char *wd) {
+        if (argc == 3)
+        {
+            if (!(strcmp(argv[1], "-p")))
             {
-                if (!(strcmp(argv[1], "-p")))
-                {
-                    char *prompt = argv[2];
-                    printf("\033[38;5;82;1m%s\033[0m:\033[38;5;45m%s\033[38;5;231m$ \033[0m", prompt, wd);
-                }
-            }
-            else if (argc == 1)
-            {
-                printf("\033[38;5;82;1m%s\033[0m:\033[38;5;45m%s\033[38;5;231m$ \033[0m", "451sh", wd);
-            }
-            else
-            {
-                printf("error: 2 argument expected but %d was given {-p <prompt>} \n", argc - 1);
+                char *prompt = argv[2];
+                printf("\033[38;2;220;50;47;1m%s\033[0m:\033[38;2;38;139;210;1m%s\033[0m$ ", prompt, wd);
             }
         }
-
-        // current working directory
-        char *pwd_command() {
-            char *cwd = (char *)calloc(512, sizeof(char));
-            getcwd(cwd, 512);
-            return cwd;
+        else if (argc == 1)
+        {
+            printf("\033[38;2;220;50;47;1m%s\033[0m:\033[38;2;38;139;210;1m%s\033[0m$ ", "451sh", wd);
         }
-
-        // command reader, returns an array of commands & args(command is 1st element)
-        char **read_command() {
-            char string[512]; /* initially input consist of 512 char but you need to change it to be dynamic */
-            scanf(" %[^\n]s", string);
-            char *token = strtok(string, " "), **string_list = (char **)malloc(10 * sizeof(char)); /* (string_list)initially the bassh accepts 1 command and 9 args */
-            for (int index = 0; token != NULL; index++)
-            {
-                string_list[index] = strdup(token);
-                token = strtok(NULL, " ");
-            }
-            return string_list;
+        else
+        {
+            printf("error: 2 argument expected but %d was given {-p <prompt>} \n", argc - 1);
         }
-
-        // change directory command [returns -1 on fail, zero on success]
-        int cd(char *path) {
-            return chdir(path);
-        }
-
-        // [returns current process ID]
-        int pid() {
-          return getpid();
-        }
-
-        // [returns Parent process ID
-        int ppid() {
-          return getppid();
-        }
-
-        // set enviroment variable [returns -1 on fail, zero on success]
-        int set(char *var_name, char *val) {
-            return setenv(var_name, val, 1);
-        }
-
-        char *get(char *var_name){
-            return getenv(var_name);
-        }
-
-        /* extra */
-            void clear() {
-                system("clear");
-            }
-
-            // unset enviroment variable [returns -1 on fail, zero on success]
-            int unset(char *var_name) {
-                return unsetenv(var_name);
-            }
-
+    }
 
     while (1) /*repeat forever*/
     {
@@ -155,9 +177,6 @@ int main(int argc, char *argv[])
             else if (commands_args_list[2] == NULL) {
                 set(commands_args_list[1], "NULL");
             }
-            else if (commands_args_list[3] != NULL) {
-                printf("bash: set: too many arguments\n");
-            }
             else{
                 set(commands_args_list[1], commands_args_list[2]);
             }
@@ -194,19 +213,20 @@ int main(int argc, char *argv[])
         }
 
         /* non-bulitin commands execution */
+        pid_t child;
         int status;
-        int fork_ret = fork();
-        if (fork_ret != 0) /* Parent process */
-        {
-            /* wait for child to exit */
-            waitpid(fork_ret, &status, 0);
+
+        child = spawn_ch_process(commands_args_list[0], commands_args_list);
+
+        if (waitpid(child, &status, WUNTRACED | WCONTINUED) == -1) {
+            // only executed in case of execve faliur to print the error
+            perror("waitpid");
+
+            exit(EXIT_FAILURE);
         }
-        else /* Child process */
-        {
-            printf("[%d] %s \n", pid(), commands_args_list[0]);
-            // execvp(...);
-            printf("Command '%s' not found, please try again\n", commands_args_list[0]);
-            exit(status);
+        if ( WIFEXITED(status) ) {
+            int es = WEXITSTATUS(status);
+            printf("\033[38;2;226;125;95;1m** \033[0mExit status was %d\033[38;2;226;125;95;1m **\033[0m\n", es);
         }
     }
 
